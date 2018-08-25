b9f07c24c24005109269eeb5a130d52aa574a48b
carnd <p.liu@gotion.local>
2018-08-25 21:25:00 +0800

    test

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -163,7 +163,7 @@ void UKF::Prediction(double delta_t) {
 	//Creat sigma points
 	Xsig_aug.col(0) = x_aug;
 	std::cout << "test point 1" << std::endl;
-	for (int i = 0; i < 2 * n_aug_ + 1; i++)
+	for (int i = 0; i < n_aug_; i++)
 	{
 		Xsig_aug.col(i + 1) = x_aug + sqrt(lambda_ + n_aug_)*L.col(i);
 		Xsig_aug.col(i + n_aug_ + 1) = x_aug - sqrt(lambda_ + n_aug_)*L.col(i);
