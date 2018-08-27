9fb9c9161f846bd35cd889239d7abd860d814b63
carnd <p.liu@gotion.local>
2018-08-27 13:57:49 +0800

    test

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -110,6 +110,7 @@ void UKF::ProcessMeasurement(MeasurementPackage meas_package) {
 	}
 	double delta_t = (meas_package.timestamp_ - time_us_) / 1000000.0;
 	time_us_ = meas_package.timestamp_;
+	std::cout << "delta_t" << delta_t << std::endl;
 	Prediction(delta_t);
 	if (meas_package.sensor_type_ == meas_package.LASER)
 	{
